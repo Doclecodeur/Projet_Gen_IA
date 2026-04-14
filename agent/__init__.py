35afddad51ae90b5ed05614fea6a3f862355b4d3
lydiaouldali1 <ouldalilydia55@gmail.com>
2026-04-14 16:52:18 +0200

    Update __init__.py
    mise a jour propre

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,1 +1,6 @@
-# package agent
+# agent package
+from agent.router import AssistantRouter
+from agent.rag_pipeline import RAGPipeline
+from agent.tools import get_all_tools
+
+__all__ = ["AssistantRouter", "RAGPipeline", "get_all_tools"]
